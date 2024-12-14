c20dee4ecbc7ae93f7589ad3a3e37ba1991d7ea4
CoolColdUK <coolcold@gmail.com>
2024-12-14 20:51:17 +0000

    add new function

--- packages/util/src/array/index.ts
+++ packages/util/src/array/index.ts
@@ -2,6 +2,8 @@ export * from './castArray';
 export * from './extractArrayLast';
 export * from './fillerArray';
 export * from './filterArrayUndefined';
+export * from './indexOfCaseInsensitive';
 export * from './padArray';
+export * from './removeDuplicateCaseInsensitive';
 export * from './toObject';
 export * from './uniqueArray';
---
+++ packages/util/src/array/indexOfCaseInsensitive.test.ts
@@ -0,0 +1,50 @@
+import {indexOfCaseInsensitive} from './indexOfCaseInsensitive';
+
+interface TestCase {
+  searchElement: string;
+  result: number;
+}
+
+describe('indexOfCaseInsensitive', () => {
+  let data: string[];
+
+  beforeEach(() => {
+    data = ['One', 'two', 'Three', 'four', 'FIVE', 'six'];
+  });
+
+  const testCases: Record<string, TestCase> = {
+    'Should find "one" case insensitively': {
+      searchElement: 'one',
+      result: 0,
+    },
+    'Should find "Two" case insensitively': {
+      searchElement: 'Two',
+      result: 1,
+    },
+    'Should find "three" case insensitively': {
+      searchElement: 'three',
+      result: 2,
+    },
+    'Should find "FOUR" case insensitively': {
+      searchElement: 'FOUR',
+      result: 3,
+    },
+    'Should find "five" case insensitively': {
+      searchElement: 'five',
+      result: 4,
+    },
+    'Should not find "seven"': {
+      searchElement: 'seven',
+      result: -1,
+    },
+    'Should not find empty string': {
+      searchElement: '',
+      result: -1,
+    },
+  };
+
+  test.each(Object.entries(testCases))('%s', (_title, testcase) => {
+    const {searchElement, result} = testcase;
+    expect(indexOfCaseInsensitive(data, searchElement)).toBe(result);
+  });
+});
