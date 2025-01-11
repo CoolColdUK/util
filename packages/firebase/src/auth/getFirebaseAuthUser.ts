e784a59c75319dc7e02a4715890b27bf518cc5da
CoolColdUK <coolcold@gmail.com>
2025-01-11 12:16:26 +0000

    fix typo

--- packages/firebase/src/auth/getFirebaseAuthUser.ts
+++ packages/firebase/src/auth/getFirebaseAuthUser.ts
@@ -4,10 +4,10 @@ import {Auth} from 'firebase-admin/auth';
  * Verify token
  * @returns decoded id token if valid, undefined otherwise
  */
-export async function getFirebaseAuthUser(firestoreAuth: Auth, token?: string) {
+export async function getFirebaseAuthUser(firebaseAuth: Auth, token?: string) {
   if (!token) return false;
   try {
-    return await firestoreAuth.verifyIdToken(token);
+    return await firebaseAuth.verifyIdToken(token);
   } catch (error) {
     return undefined;
   }
