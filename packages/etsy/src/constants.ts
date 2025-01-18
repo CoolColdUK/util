1e7840b85841ba2274311996198837e560959360
CoolColdUK <coolcold@gmail.com>
2025-01-18 14:54:03 +0000

    update to use redirect

--- packages/etsy/src/constants.ts
+++ packages/etsy/src/constants.ts
@@ -1,7 +1,8 @@
 export const PORT = 3003;
 export const SERVER = 'http://localhost';
 export const SERVER_URL = SERVER + ':' + PORT;
-export const OAUTH_REDIRECT = '/oauth/callback';
+export const OAUTH_REDIRECT = '/oauth/redirect';
+export const OAUTH_CALLBACK = '/oauth/callback';
 
 export const ETSY_API_KEY = process.env['ETSY_API_KEY'] || '';
 export const ETSY_API_SECRET = process.env['ETSY_API_SECRET'] || '';
--- packages/etsy/src/server.ts
+++ packages/etsy/src/server.ts
@@ -1,7 +1,7 @@
 // Import the express library
 import {buildPkce} from '@coolcolduk/util';
 import express from 'express';
-import {ETSY_API_KEY, OAUTH_REDIRECT, PORT, SERVER_URL, stateStore} from './constants';
+import {ETSY_API_KEY, OAUTH_CALLBACK, OAUTH_REDIRECT, PORT, SERVER_URL, stateStore} from './constants';
 import {createEndpointPing} from './endpoint/createEndpointPing';
 import {runEndpointOauthCallback} from './endpoint/runEndpointOauthCallback';
 import {EtsyScopeEnum} from './enum/EtsyScopeEnum';
@@ -11,17 +11,19 @@ const app = express();
 
 // Send a "Hello World!" response to a default get request
 app.get('/', (_req, res) => {
-  const data = buildPkce();
-  stateStore['state'] = data;
   res.set('Content-Type', 'text/html');
-  res.send(
-    `<a href="${buildEtsyOauthUrl(ETSY_API_KEY, SERVER_URL + OAUTH_REDIRECT, EtsyScopeEnum.EMAIL_READ, data)}">login</a>`,
-  );
+  res.send(`<a href="${OAUTH_REDIRECT}">login</a>`);
 });
 
 app.get('/ping', createEndpointPing(ETSY_API_KEY));
 
-app.get(OAUTH_REDIRECT, (req, res) => {
+app.get(OAUTH_REDIRECT, (_req, res) => {
+  const data = buildPkce();
+  stateStore['state'] = data;
+  res.redirect(buildEtsyOauthUrl(ETSY_API_KEY, SERVER_URL + OAUTH_REDIRECT, EtsyScopeEnum.EMAIL_READ, data));
+});
+
+app.get(OAUTH_CALLBACK, (req, res) => {
   runEndpointOauthCallback(
     res,
     ETSY_API_KEY,
