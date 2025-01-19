2ee0a70c468395ee3652700a815590be60a35651
CoolColdUK <coolcold@gmail.com>
2025-01-19 00:55:33 +0000

    function allow buffer or string

--- packages/util/src/pkceCodeChallenge/encodeBase64URL.ts
+++ packages/util/src/pkceCodeChallenge/encodeBase64URL.ts
@@ -3,6 +3,7 @@
  * @param buffer
  * @returns
  */
-export function encodeBase64URL(buffer: Buffer) {
-  return buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+export function encodeBase64URL(buffer: Buffer | string) {
+  const str = typeof buffer === 'string' ? buffer : buffer.toString('base64');
+  return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 }
