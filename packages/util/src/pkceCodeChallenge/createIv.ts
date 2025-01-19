b9bb90f38dabbb97710561ac0f3efd0d41721b33
CoolColdUK <coolcold@gmail.com>
2025-01-19 00:55:53 +0000

    use 12 byte

--- packages/util/src/pkceCodeChallenge/createIv.ts
+++ packages/util/src/pkceCodeChallenge/createIv.ts
@@ -2,5 +2,5 @@ import crypto from 'crypto';
 import {encodeBase64URL} from './encodeBase64URL';
 
 export default function createIv(): string {
-  return encodeBase64URL(crypto.randomBytes(32));
+  return encodeBase64URL(crypto.randomBytes(12));
 }
