643e067130fc94fb3c9d2d8107ae59f9923c1f63
CoolColdUK <coolcold@gmail.com>
2025-01-24 00:03:15 +0000

    add new enum

---
+++ packages/etsy/src/enum/EtsyListingIncludesEnum.ts
@@ -0,0 +1,12 @@
+/**
+ * Enum for listing includes options
+ */
+export enum EtsyListingIncludesEnum {
+  SHIPPING = 'Shipping',
+  IMAGES = 'Images',
+  SHOP = 'Shop',
+  USER = 'User',
+  TRANSLATIONS = 'Translations',
+  INVENTORY = 'Inventory',
+  VIDEOS = 'Videos',
+}
---
+++ packages/etsy/src/enum/EtsyListingTranslateEnum.ts
@@ -0,0 +1,14 @@
+/**
+ * Enum for translation language options
+ */
+export enum EtsyListingTranslateEnum {
+  DE = 'de',
+  EN = 'en',
+  ES = 'es',
+  FR = 'fr',
+  IT = 'it',
+  JA = 'ja',
+  NL = 'nl',
+  PL = 'pl',
+  PT = 'pt',
+}
