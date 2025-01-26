f810caf25ac297820b1d15aa570f1c1ad982b8a4
CoolColdUK <coolcold@gmail.com>
2025-01-26 16:12:54 +0000

    add access token again for get etsy shop by owner user id

--- packages/etsy/src/request/shop/getEtsyShopByOwnerUserId.ts
+++ packages/etsy/src/request/shop/getEtsyShopByOwnerUserId.ts
@@ -6,9 +6,14 @@ import getEtsyRequestAxiosConfig from '../../util/getEtsyRequestAxiosConfig'; //
  * Fetches the shop information based on the owner's user ID.
  * @see https://developers.etsy.com/documentation/reference#operation/getShopByOwnerUserId
  * @param apiKey - The API key
+ * @param accessToken - The OAuth2 access token for authorization.
  * @param userId - The user ID of the shop owner.
  * @returns Shop details as a promise of EtsyShop.
  */
-export function getEtsyShopByOwnerUserId(apiKey: string, userId: number): Promise<AxiosResponse<EtsyShop>> {
-  return axios.get<EtsyShop>(`/application/users/${userId}/shops`, getEtsyRequestAxiosConfig({apiKey}));
+export function getEtsyShopByOwnerUserId(
+  apiKey: string,
+  accessToken: string,
+  userId: number,
+): Promise<AxiosResponse<EtsyShop>> {
+  return axios.get<EtsyShop>(`/application/users/${userId}/shops`, getEtsyRequestAxiosConfig({apiKey, accessToken}));
 }
