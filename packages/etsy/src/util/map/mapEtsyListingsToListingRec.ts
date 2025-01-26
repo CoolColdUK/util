9a07ef1d443bff9257b83440cc1f84c6a8e27aba
CoolColdUK <coolcold@gmail.com>
2025-01-26 21:59:21 +0000

    add comment

--- packages/etsy/src/util/map/mapEtsyListingsToListingRec.ts
+++ packages/etsy/src/util/map/mapEtsyListingsToListingRec.ts
@@ -2,6 +2,11 @@ import {toObject} from '@coolcolduk/util';
 import {EtsyListing} from '../../interfaces/EtsyListing';
 import {EtsyListings} from '../../interfaces/EtsyListings';
 
+/**
+ * Convert etsy listings to Etsy listing record with listing id as key
+ * @param etsyListings
+ * @returns
+ */
 export function mapEtsyListingsToListingRec(etsyListings: EtsyListings): Record<string, EtsyListing> {
   return toObject(etsyListings.results, 'listing_id');
 }
