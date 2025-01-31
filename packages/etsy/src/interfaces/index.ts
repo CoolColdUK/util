3390d23fa9617cba99ca458dcfaa675014281b10
CoolColdUK <coolcold@gmail.com>
2025-01-31 19:52:18 +0000

    fix build

--- packages/etsy/src/interfaces/index.ts
+++ packages/etsy/src/interfaces/index.ts
@@ -1,13 +1,13 @@
 export * from './EtsyListing';
 export * from './EtsyListingImage';
 export * from './EtsyListingInventory ';
-export * from './EtsyListings';
 export * from './EtsyListingVideo';
 export * from './EtsyPrice';
 export * from './EtsyProduct';
 export * from './EtsyProductionPartner';
 export * from './EtsyProductOffering';
 export * from './EtsyProductPropertyValue';
+export * from './EtsyResponse';
 export * from './EtsyShippingProfile';
 export * from './EtsyShippingProfileDestination';
 export * from './EtsyShippingProfileUpgrade ';
--- packages/etsy/src/util/map/mapEtsyListingsToListingRec.ts
+++ packages/etsy/src/util/map/mapEtsyListingsToListingRec.ts
@@ -1,12 +1,12 @@
 import {toObject} from '@coolcolduk/util';
 import {EtsyListing} from '../../interfaces/EtsyListing';
-import {EtsyListings} from '../../interfaces/EtsyListings';
+import {EtsyList} from '../../interfaces/EtsyResponse';
 
 /**
  * Convert etsy listings to Etsy listing record with listing id as key
  * @param etsyListings
  * @returns
  */
-export function mapEtsyListingsToListingRec(etsyListings: EtsyListings): Record<string, EtsyListing> {
+export function mapEtsyListingsToListingRec(etsyListings: EtsyList<EtsyListing>): Record<string, EtsyListing> {
   return toObject(etsyListings.results, 'listing_id');
 }
