af71c0da28e0870795386f163eb47ffeb626c870
CoolColdUK <coolcold@gmail.com>
2025-02-01 14:17:17 +0000

    add return refresh token

--- packages/etsy/src/util/helper/withEtsyRefreshToken.ts
+++ packages/etsy/src/util/helper/withEtsyRefreshToken.ts
@@ -1,18 +1,25 @@
 import {retry} from '@coolcolduk/util';
 import {refreshEtsyAccessToken} from '../../request/login/refreshEtsyAccessToken';
 
+export interface WithEtsyRefreshTokenResult<T> {
+  accessToken: string;
+  refreshToken: string;
+  result: T;
+}
+
 export async function withEtsyRefreshToken<T>(
   etsyApiKey: string,
   accessToken: string,
   refreshToken: string,
   fn: (accessToken: string) => Promise<T>,
-): Promise<{accessToken: string; result: T}> {
+): Promise<WithEtsyRefreshTokenResult<T>> {
   return retry(
     async (i) => {
       const refreshedToken = i === 1 ? (await refreshEtsyAccessToken(etsyApiKey, refreshToken)).data : undefined;
       const finalAccessToken = refreshedToken?.access_token || accessToken;
+      const finalRefreshToken = refreshedToken?.refresh_token || refreshToken;
 
-      return {accessToken: finalAccessToken, result: await fn(finalAccessToken)};
+      return {accessToken: finalAccessToken, refreshToken: finalRefreshToken, result: await fn(finalAccessToken)};
     },
     {
       numberOfTries: 2,
