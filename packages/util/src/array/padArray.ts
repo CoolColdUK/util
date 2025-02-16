61f92bd8b628a8ba15ede0b6d5fc5591f5a3a74b
CoolColdUK <coolcold@gmail.com>
2025-02-16 23:33:41 +0000

    default value

--- packages/util/src/array/padArray.ts
+++ packages/util/src/array/padArray.ts
@@ -5,10 +5,10 @@ import {fillerArray} from './fillerArray';
  * @param arr original array
  * @param padValue value to use for padding
  * @param requiredSize array length required
- * @param prefix padding add to front when true, to back when false
+ * @param prefix [prefix=false] padding add to front when true, to back when false
  * @returns original array when `requiredSize` smaller than array size, otherwise padded array
  */
-export function padArray<T>(arr: T[], padValue: T, requiredSize: number, prefix = true): T[] {
+export function padArray<T>(arr: T[], padValue: T, requiredSize: number, prefix = false): T[] {
   if (arr.length >= requiredSize) return arr;
   const requiredPad = requiredSize - arr.length;
   const padArr = fillerArray(padValue, requiredPad);
