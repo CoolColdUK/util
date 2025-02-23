060cdf6dd133c1fd8f98a1172f56a24ed375c7d8
CoolColdUK <coolcold@gmail.com>
2025-02-23 02:09:30 +0000

    add build form data

---
+++ packages/etsy/src/util/builder/buildFormData.ts
@@ -0,0 +1,14 @@
+/**
+ * build form data for file transfer
+ * @param data
+ * @returns
+ */
+export function buildFormData(data: Record<string, any>) {
+  const formData = new FormData();
+
+  Object.entries(data).forEach(([key, value]) => {
+    formData.append(key, value);
+  });
+
+  return formData;
+}
