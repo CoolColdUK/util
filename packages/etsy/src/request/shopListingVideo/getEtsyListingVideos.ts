5de2d05dbc8fd52cb0f5a0d336332c3f01c1e38f
CoolColdUK <coolcold@gmail.com>
2025-02-24 20:50:22 +0000

    add getEtsyListingVideos

---
+++ packages/etsy/src/request/shopListingVideo/getEtsyListingVideos.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import {EtsyListingVideo} from '../../interfaces';
+import {EtsyList, EtsyResponseMany} from '../../interfaces/EtsyResponse';
+import getEtsyRequestAxiosConfig from '../../util/getEtsyRequestAxiosConfig';
+
+/**
+ * Retrieves all videos associated with an Etsy listing.
+ * @see https://developers.etsy.com/documentation/reference/#operation/getListingVideos
+ * @param apiKey - The API key.
+ * @param accessToken - The OAuth2 access token.
+ * @param listingId - The numeric ID of the listing to retrieve videos for (required, integer >= 1).
+ * @returns The response containing an array of video details for the listing.
+ * @throws {Error} If listingId is not a positive integer.
+ */
+export function getEtsyListingVideos(
+  apiKey: string,
+  accessToken: string,
+  listingId: number, // Required path parameter, integer >= 1
+): EtsyResponseMany<EtsyListingVideo> {
+  // Validate listingId
+  if (!Number.isInteger(listingId) || listingId < 1) {
+    throw new Error('listingId must be an integer >= 1');
+  }
+
+  // Make the API request without query parameters
+  return axios.get<EtsyList<EtsyListingVideo>>(
+    `/application/listings/${listingId}/videos`,
+    getEtsyRequestAxiosConfig({accessToken, apiKey}),
+  );
+}
--- packages/etsy/src/request/shopListingVideo/index.ts
+++ packages/etsy/src/request/shopListingVideo/index.ts
@@ -1,1 +1,2 @@
+export * from './getEtsyListingVideos';
 export * from './uploadEtsyListingVideo';
