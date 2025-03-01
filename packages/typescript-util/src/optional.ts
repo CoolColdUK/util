8778bd47469fce82cfab77293bff4b0e90e9fd23
CoolColdUK <coolcold@gmail.com>
2025-03-01 12:25:37 +0000

    add non nullable by

--- packages/typescript-util/src/optional.ts
+++ packages/typescript-util/src/optional.ts
@@ -14,3 +14,8 @@ export type RequiredBy<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
  * Make certain key nullable
  */
 export type NullableBy<T, K extends keyof T> = Omit<T, K> & NullablePartial<Pick<T, K>>;
+
+/**
+ * Make certain key nullable
+ */
+export type NonNullableBy<T, K extends keyof T> = Omit<T, K> & NonNullable<Pick<T, K>>;
