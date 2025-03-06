402e85cfdacb42bd4e336852cb3ce58bd8a92bd5
CoolColdUK <coolcold@gmail.com>
2025-03-06 23:12:31 +0000

    add create apollo helper

---
+++ packages/gql/src/createApolloServer.ts
@@ -0,0 +1,51 @@
+import {ApolloServer, ApolloServerPlugin, BaseContext, ContextFunction} from '@apollo/server';
+import {ExpressContextFunctionArgument, expressMiddleware} from '@apollo/server/express4';
+import {Logger} from '@coolcolduk/enum';
+import cors, {CorsOptions} from 'cors';
+import express, {Handler} from 'express';
+import {GraphQLDirective, GraphQLFormattedError} from 'graphql';
+import {buildSchema, NonEmptyArray} from 'type-graphql';
+import {BuildContextOptions} from 'type-graphql/build/typings/schema/build-context';
+
+export interface CreateApolloServerOptions<TContext extends BaseContext> {
+  cors?: CorsOptions;
+  authChecker?: BuildContextOptions['authChecker'];
+  directives?: GraphQLDirective[];
+  // errorHandler?: ErrorRequestHandler;
+  middleware?: Handler[];
+  logger?: Logger;
+  formatError?: (formattedError: GraphQLFormattedError, error: unknown) => GraphQLFormattedError;
+  plugins?: ApolloServerPlugin<TContext>[];
+  context?: ContextFunction<[ExpressContextFunctionArgument], TContext>;
+}
+
+export async function createApolloServer<TContext extends BaseContext>(
+  resolvers: NonEmptyArray<Function>,
+  options: CreateApolloServerOptions<TContext> = {},
+) {
+  const {cors: corsOptions, authChecker, directives, middleware, logger, formatError, plugins, context} = options;
+  const app = express();
+
+  // Build a GraphQL schema using TypeGraphQL
+  const schema = await buildSchema({
+    resolvers,
+    authChecker,
+    directives,
+  });
+
+  // Initialize Apollo Server with the schema
+  const server = new ApolloServer<TContext>({
+    schema,
+    logger,
+    formatError,
+    plugins,
+  });
+
+  await server.start();
+
+  if (middleware) app.use(middleware);
+
+  app.use('/', cors(corsOptions), expressMiddleware(server as unknown as ApolloServer<BaseContext>, {context}));
+
+  return app;
+}
