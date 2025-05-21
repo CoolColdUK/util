1913989556439f89b93a5464ec077978a85bb3d1
CoolColdUK <coolcold@gmail.com>
2025-05-21 18:56:04 +0100

    allow user declare token type

--- packages/firebase/src/auth/getFirebaseAuthUser.ts
+++ packages/firebase/src/auth/getFirebaseAuthUser.ts
@@ -4,10 +4,13 @@ import {Auth, DecodedIdToken} from 'firebase-admin/auth';
  * Verify token
  * @returns decoded id token if valid, undefined otherwise
  */
-export async function getFirebaseAuthUser(firebaseAuth: Auth, token?: string): Promise<DecodedIdToken | undefined> {
+export async function getFirebaseAuthUser<R extends DecodedIdToken = DecodedIdToken>(
+  firebaseAuth: Auth,
+  token?: string,
+): Promise<R | undefined> {
   if (!token) return undefined;
   try {
-    return await firebaseAuth.verifyIdToken(token);
+    return (await firebaseAuth.verifyIdToken(token)) as R;
   } catch (error) {
     return undefined;
   }
