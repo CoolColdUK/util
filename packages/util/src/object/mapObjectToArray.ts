604cd933ffd5f0c1578a2acc004a9f405222b748
CoolColdUK <coolcold@gmail.com>
2025-05-24 11:47:42 +0100

    accept record string,number in mapobjecttoarray

--- packages/util/src/object/mapObjectToArray.ts
+++ packages/util/src/object/mapObjectToArray.ts
@@ -4,6 +4,6 @@
  * @param spacer - The character to use between key and value (default: '=')
  * @returns An array of strings in the format "key=value"
  */
-export function mapObjectToArray(obj: Record<string, string>, spacer: string = '='): string[] {
+export function mapObjectToArray(obj: Record<string, string | number>, spacer: string = '='): string[] {
   return Object.entries(obj).map(([key, value]) => `${key}${spacer}${value}`);
 }
