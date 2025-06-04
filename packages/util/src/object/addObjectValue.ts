a5e4810fc81fa89e3a513f8cdaaeef2cbb84a7e1
CoolColdUK <coolcold@gmail.com>
2025-06-04 01:34:43 +0100

    update typings

--- packages/util/src/object/addObjectValue.ts
+++ packages/util/src/object/addObjectValue.ts
@@ -4,7 +4,7 @@
  * @param obj2 - The object to add the value from
  * @returns The new object with the added value
  */
-export function addObjectValue(obj1: Record<string, number>, obj2: Record<string, number>): Record<string, number> {
+export function addObjectValue<T extends Record<string, number>>(obj1: T, obj2: T): T {
   return Object.entries(obj1).reduce((acc, [key, value]) => {
     return {
       ...acc,
