ab11cfcbb723f8accf791a3052cc9467711d804e
CoolColdUK <coolcold@gmail.com>
2025-07-28 20:39:34 +0100

    rename fn

--- packages/util/src/promise/mapPromiseLimit.ts
+++ packages/util/src/promise/mapPromiseLimit.ts
@@ -8,6 +8,6 @@ import {mapLimit as asyncMapLimit} from 'async';
  * @deprecated does not work yet, need more testing
  * @returns
  */
-export async function mapLimit<T, R>(arr: T[], func: (data: T) => Promise<R>, limit: number): Promise<R[]> {
+export async function mapPromiseLimit<T, R>(arr: T[], func: (data: T) => Promise<R>, limit: number): Promise<R[]> {
   return asyncMapLimit(arr, limit, func);
 }
