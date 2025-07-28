8bd837b799928d8aa2b369a4d5d8806552f9059f
CoolColdUK <coolcold@gmail.com>
2025-07-28 02:51:34 +0100

    add key filter

--- packages/util/src/object/transformObjectValue.ts
+++ packages/util/src/object/transformObjectValue.ts
@@ -1,11 +1,19 @@
 /**
  * execute function in all values in record
- * @param rec
- * @param fn
+ * @param rec - record to transform
+ * @param fn - function to apply to each value
+ * @param keyFilter - if provided, only apply function to keys in this array
  * @returns
  */
-export function transformObjectValue<T, U>(rec: Record<string, T>, fn: (d: T) => U): Record<string, U> {
+export function transformObjectValue<T, U>(
+  rec: Record<string, T>,
+  fn: (d: T) => U,
+  keyFilter?: string[],
+): Record<string, U> {
   const data = Object.entries(rec);
-  const result: [string, U][] = data.map(([key, val]) => [key, fn(val)]);
+  const result: [string, U][] = data.map(([key, val]) => [
+    key,
+    (!keyFilter || keyFilter.includes(key) ? fn(val) : val) as U,
+  ]);
   return Object.fromEntries(result);
 }
