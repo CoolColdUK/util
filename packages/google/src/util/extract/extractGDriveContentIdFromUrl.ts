2eec31ac6a5db6a42c89484e1d2c4a4487013456
CoolColdUK <coolcold@gmail.com>
2025-07-28 23:36:34 +0100

    remove lodash

--- packages/google/src/util/extract/extractGDriveContentIdFromUrl.ts
+++ packages/google/src/util/extract/extractGDriveContentIdFromUrl.ts
@@ -1,5 +1,3 @@
-import {get} from 'lodash';
-
 /**
  * function to extract content id/file id from google drive url (e.g. https://drive.google.com/file/d/1234567890/view?usp=sharing)
  * @param url
@@ -7,5 +5,5 @@ import {get} from 'lodash';
  */
 export function extractGDriveContentIdFromUrl(url: string): string | undefined {
   const match = url.match(/\/d\/([a-zA-Z0-9_-]+)/);
-  return get(match, '1', undefined);
+  return match?.[1];
 }
--- packages/google/src/util/extract/extractGDriveFolderIdFromUrl.ts
+++ packages/google/src/util/extract/extractGDriveFolderIdFromUrl.ts
@@ -1,6 +1,4 @@
-import {get} from 'lodash';
-
 export function extractGDriveFolderIdFromUrl(url: string): string | undefined {
   const match = url.match(/folders\/([a-zA-Z0-9_-]+)/);
-  return get(match, '1', undefined);
+  return match?.[1];
 }
