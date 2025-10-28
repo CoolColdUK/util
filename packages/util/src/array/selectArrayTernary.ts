f6b146b428560a5f6e1156fc90442ba86009a0e4
CoolColdUK <coolcold@gmail.com>
2025-10-28 23:12:41 +0000

    add another condition

--- packages/util/src/array/selectArrayTernary.ts
+++ packages/util/src/array/selectArrayTernary.ts
@@ -3,13 +3,16 @@
  * Useful for replacing nested ternary expressions.
  *
  * @param conditions - An array of boolean conditions.
- * @param outputs - An array of outputs corresponding to each condition.
+ * @param outputs - An array of outputs corresponding to each condition. should be the same length as conditions.
  * @param defaultValue - The value to return if no conditions are true.
  * @returns The output corresponding to the first true condition, or undefined if none are true.
  */
 export function selectArrayTernary<T>(conditions: boolean[], outputs: T[], defaultValue: T): T {
   if (outputs.length === 0) {
     throw new Error('selectArrayTernary: outputs array must not be empty');
+  }
+  if (conditions.length !== outputs.length) {
+    throw new Error('selectArrayTernary: conditions and outputs arrays must be the same length');
   }
   const idx = conditions.indexOf(true);
   if (idx !== -1 && idx < outputs.length) {
