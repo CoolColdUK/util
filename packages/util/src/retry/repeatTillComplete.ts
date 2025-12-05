2f083af369b95959a9ba713db58826b07025df8b
CoolColdUK <coolcold@gmail.com>
2025-12-05 19:18:49 +0000

    update function to allow promise

--- packages/util/src/retry/repeatTillComplete.ts
+++ packages/util/src/retry/repeatTillComplete.ts
@@ -1,23 +1,31 @@
 import {MaybePromise} from '@coolcolduk/typescript-util';
-import {sleepMs} from '../sleep';
+import {sleepMs} from '../sleep/sleepMs';
 
+/**
+ *
+ * @param fetchFunction - The function to repeat. if result undersired, it should return undefined
+ * @param isComplete
+ * @param shouldRetry
+ * @param delayMs
+ * @returns
+ */
 export async function repeatTillComplete<T>(
   fetchFunction: (prev?: T) => MaybePromise<T>,
-  isComplete: (data: T) => boolean,
-  shouldRetry: (attemptCount: number, data?: T) => boolean,
+  isComplete: (data: T) => MaybePromise<boolean>,
+  shouldRetry: (attemptCount: number, data?: T) => MaybePromise<boolean>,
   delayMs: number = 1000,
 ): Promise<T> {
   let attempts = 0;
 
   let result: T | undefined;
-  while (shouldRetry(attempts, result)) {
+  while (await shouldRetry(attempts, result)) {
     result = await fetchFunction(result);
 
-    if (isComplete(result)) {
+    if (await isComplete(result)) {
       return result;
     }
 
-    attempts++;
+    attempts += 1;
     await sleepMs(delayMs);
   }
 
