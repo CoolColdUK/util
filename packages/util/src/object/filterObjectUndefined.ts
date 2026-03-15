3eeb855b6cfc0b64d22ad98de4e2b76b4ee39b5a
CoolColdUK <coolcold@gmail.com>
2026-03-15 16:24:24 +0000

    update typing

--- packages/util/src/object/filterObjectUndefined.ts
+++ packages/util/src/object/filterObjectUndefined.ts
@@ -6,6 +6,6 @@ import {filterObject} from './filterObject';
  * @param obj The object to filter.
  * @returns A new object without keys that have undefined values.
  */
-export function filterObjectUndefined<T>(obj: Record<string, May<T>>): Record<string, T> {
-  return filterObject(obj, (_key, value) => value !== undefined) as Record<string, T>;
+export function filterObjectUndefined<T>(obj: Record<string, May<T>>): {[K in keyof T]: T[K]} {
+  return filterObject(obj, (_key, value) => value !== undefined) as {[K in keyof T]: T[K]};
 }
